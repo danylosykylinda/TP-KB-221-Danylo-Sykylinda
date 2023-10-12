725b9150e76264ddb92e74704ccdb7e20a6b7b1c
unknown <danilosikilinda@gmail.com>
2023-10-12 09:55:58 +0300

    Task1 and Task2

--- topic_04/task_1.py
+++ topic_04/task_1.py
@@ -1,50 +1,51 @@
 def plus(x, y):
-    try:
-        return float(x) + float(y)
-    except ValueError:
-        print("ERROR!!! You entered something wrong, please enter it again.")
+    return float(x) + float(y)
 
 def minus(x, y):
-    try:
-        return float(x) - float(y)
-    except ValueError:
-        print("ERROR!!! You entered something wrong, please enter it again.")
+    return float(x) - float(y)
+    
 
 def multiply(x, y):
-    try:
-        return float(x) * float(y)
-    except ValueError:
-        print("ERROR!!! You entered something wrong, please enter it again.")
+    return float(x) * float(y)
 
 def divide(x, y):
     try:
         return float(x) / float(y)
-    except ValueError:
-        print("ERROR!!! You entered something wrong, please enter it again.")
     except ZeroDivisionError:
         print("ERROR!!! You are trying to divide a number by zero, please enter it again")
 
-while True:
-    a = (input("Enter the first number or 'q' to quit: \n"))
-    if a == 'q' or a == 'Q':
-        print("You have left the program")
-        break
-    b = (input("Enter the second number or 'q' to quit: \n"))
-    if b == 'q' or b == 'Q':
-        print("You have left the program")
-        break
-    op = input("Choose an operation (+, -, *, /) or 'q' to quit: \n")
-    if op == 'q' or op == 'Q':
-        print("You have left the program")
-        break
-    if op == '+':
-        print(f"Answer: {plus(a, b)}")
-    elif op == '-':
-        print(f"Answer: {minus(a, b)}")
-    elif op == '*':
-        print(f"Answer: {multiply(a, b)}")
-    elif op == '/':
-        print(f"Answer: {divide(a, b)}")
-    else:
-        print("You entered something wrong! No operation can be done.")
+def find_excep():
+    while True:
+        try:
+            a = input("Enter the first number or 'q' to quit: \n")
+            if a == 'q' or a == 'Q':
+                print("You have left the program")
+                break
+            else:
+                float(a)
+            b = input("Enter the second number or 'q' to quit: \n")
+            if b == 'q' or b == 'Q':
+                print("You have left the program")
+                break
+            else:
+                float(b)
+            op = input("Choose an operation (+, -, *, /) or 'q' to quit: \n")
+            if op == 'q' or op == 'Q':
+                print("You have left the program")
+                break
+            if op == '+':
+                print(f"Answer: {plus(a, b)}")
+            elif op == '-':
+                print(f"Answer: {minus(a, b)}")
+            elif op == '*':
+                print(f"Answer: {multiply(a, b)}")
+            elif op == '/':
+                print(f"Answer: {divide(a, b)}")
+            else:
+                print("You entered something wrong! No operation can be done.")
+        except ValueError:
+            print("ERROR!!! You entered something wrong, please enter it again.")
+find_excep()
+
         
+            
