a854541f05a61f08809ad9c4a3ab542ad06c5f77
unknown <danilosikilinda@gmail.com>
2023-12-06 22:09:51 +0200

    Solution for lab_02

---
+++ lab_02/lab_02.py
@@ -0,0 +1,136 @@
+import csv
+import sys
+
+def load_csv(file_name):
+    list_students = []
+    try:
+        with open(file_name, 'r') as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                list_students.append(row)
+        print("Data loaded successfully!")
+        return list_students
+    except FileNotFoundError:
+        print("File not found!")
+
+def save_csv(file_name, list_students):
+    with open(file_name, 'w', newline='') as file:
+        fieldnames = ["name", "phone", "email", "group"]
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
+        for student in list_students:
+            student_data = {
+                "name": student["name"],
+                "phone": student["phone"],
+                "email": student["email"],
+                "group": student["group"]
+            }
+            writer.writerow(student_data)
+
+
+def printAllList(list_students):
+    for elem in list_students:
+        strForPrint = "Student name is " + elem["name"] + ", Phone is " + elem["phone"] + ", Email: " + elem["email"] + ", Group: " + elem["group"]
+        print(strForPrint)
+
+
+def addNewElement(list_students):
+    name = input("Please enter student name: ")
+    phone = input("Please enter student phone: ")
+    email = input("Please enter student email: ")
+    group = input("Please enter student group: ")
+    newItem = {"name": name, "phone": phone, "email": email, "group": group}
+    # find insert position
+    insertPosition = 0
+    for item in list_students:
+        if name > item["name"]:
+            insertPosition += 1
+        else:
+            break
+    list_students.insert(insertPosition, newItem)
+    print("New element has been added")
+
+
+def deleteElement(list_students):
+    name = input("Please enter name to be deleted: ")
+    deletePosition = -1
+    for item in list_students:
+        if name == item["name"]:
+            deletePosition = list_students.index(item)
+            break
+    if deletePosition == -1:
+        print("Element was not found")
+    else:
+        list_students.pop(deletePosition)
+        print("Element has been deleted")
+
+
+def updateElement(list_students):
+    name = input("Please enter name to be updated: ")
+    name_update = input("Please enter new name for this student: ")
+    deletePosition = -1
+    for item in list_students:
+        if name_update == name == item["name"]:
+            phone_update = input("Please enter new phone for this student: ")
+            email_update = input("Please enter new email for this student: ")
+            group_update = input("Please enter new group for this student: ")
+            item["phone"] = phone_update
+            item["email"] = email_update
+            item["group"] = group_update
+            print("Element has been updated")
+            deletePosition = -2
+            break
+        elif name_update != item["name"] and name == item["name"]:
+            deletePosition = list_students.index(item)
+            if deletePosition == -1:
+                print("Student is not found. Please try again correctly.")
+            else:
+                list_students.pop(deletePosition)
+            phone_update = input("Please enter new phone for this student: ")
+            email_update = input("Please enter new email for this student: ")
+            group_update = input("Please enter new group for this student: ")
+            newItem = {"name": name_update, "phone": phone_update, "email": email_update, "group": group_update}
+            insertPosition = 0
+            for item_up in list_students:
+                if name_update > item_up["name"]:
+                    insertPosition += 1
+                else:
+                    break
+            list_students.insert(insertPosition, newItem)
+            print("Element has been updated")
+            break
+    if deletePosition == -1:
+        print("Student is not found. Please try again correctly.")
+
+
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide the file name as a command line argument!")
+        return
+    data = load_csv(sys.argv[1])
+    while True:
+        chouse = input("Please specify the action [ C create, U update, D delete, P print,  X exit ] ")
+        match chouse:
+            case "C" | "c":
+                print("New element will be created:")
+                addNewElement(data)
+                printAllList(data)
+            case "U" | "u":
+                print("Existing element will be updated")
+                updateElement(data)
+                printAllList(data)
+            case "D" | "d":
+                print("Element will be deleted")
+                deleteElement(data)
+            case "P" | "p":
+                print("List will be printed")
+                printAllList(data)
+            case "X" | "x":
+                save_csv(sys.argv[1], data)
+                print("CSV file is rewritten")
+                print("Exit()")
+                break
+            case _:
+                print("Wrong chouse")
+if __name__ == '__main__':
+    main()
---
+++ lab_02/test.py
@@ -0,0 +1,46 @@
+import unittest
+from unittest.mock import patch
+from lab_02 import *
+
+class TestLab2(unittest.TestCase):
+    def setUp(self):
+        self.list_students = [
+    {"name": "Bob", "phone": "0631234567", "email": "bob@gmail.com", "group": "CS-221"},
+    {"name": "Emma", "phone": "0637457545", "email": "emma@gmail.com", "group": "CS-221"},
+    {"name": "Michael", "phone": "0994747444", "email": "michael@gmail.com", "group": "AB-219"},
+    {"name": "Zak", "phone": "0635545467", "email": "zak@gmail.com", "group": "CE-221"},
+]
+        
+    def test_addNewElement(self):
+        with patch('builtins.input', side_effect=['Denys', '0673838838', 'denys@gmail.com', 'CS-221']):
+            addNewElement(self.list_students)
+        self.assertEqual(self.list_students[1]["name"], "Denys")
+    
+
+    def test_updateElement(self):
+        with patch('builtins.input', side_effect=['Emma', 'William', '06757577575', 'william@gmail.com', 'CS-221']):
+            updateElement(self.list_students)
+        self.assertEqual(self.list_students[2]["name"], "William")
+        self.assertNotIn({"name": "Emma", "phone": "0637457545", "email": "emma@gmail.com", "group": "CS-221"}, self.list_students)
+        
+    
+    def test_deleteElement(self): 
+        with patch('builtins.input', side_effect=['Michael']):
+            deleteElement(self.list_students)
+        self.assertNotIn({"name": "Michael", "phone": "0994747444", "email": "michael@gmail.com", "group": "AB-219"}, self.list_students)
+
+    def test_save_csv(self):
+        #save_csv('test_lab2.csv', self.list_students)
+        self.assertListEqual(self.list_students,
+            [
+            {"name": "Bob", "phone": "0631234567", "email": "bob@gmail.com", "group": "CS-221"},
+            {"name": "Denys", "phone": "0673838838", "email": "denys@gmail.com", "group": "CS-221"},
+            {"name": "Emma", "phone": "0637457545", "email": "emma@gmail.com", "group": "CS-221"},
+            {"name": "William", "phone": "06757577575", "email": "william@gmail.com", "group": "CS-221"},
+            {"name": "Zak", "phone": "0635545467", "email": "zak@gmail.com", "group": "CE-221"}
+            ])
+
+if __name__ == '__main__':
+    unittest.main()
+
+    
